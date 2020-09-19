ba34fca3a5514d80eb2da9cfd0681c1ce0ba996a
Vilmos Feher <jupposessho@gmail.com>
2020-09-19 16:34:43 +0200

    Add consumer and access token to Twitter config

--- connected/src/main/scala/jupposessho/config/Configuration.scala
+++ connected/src/main/scala/jupposessho/config/Configuration.scala
@@ -1,5 +1,6 @@
 package jupposessho.config
 
+import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
 import pureconfig.{ConfigReader, ConfigSource}
 import pureconfig.generic.auto._
 import scala.concurrent.duration.FiniteDuration
@@ -12,7 +13,7 @@ object Configuration {
 
   implicit val zioDurationReader = ConfigReader[FiniteDuration].map(Duration.fromScala)
 
-  final case class TwitterConfig(retryCount: Int, duration: Duration)
+  final case class TwitterConfig(consumer: ConsumerToken, access: AccessToken, retryCount: Int, duration: Duration)
   final case class GithubConfig(url: String, retryCount: Int, duration: Duration)
   final case class ServerConfig(host: String, port: Int)
 
